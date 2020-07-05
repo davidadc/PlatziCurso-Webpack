ba0a33d812dd581239426d6872ae0bd316563d6d
David Delgado <dadch13.14@gmail.com>
2020-07-05 16:55:46 -0400

    Servidor de desarrollo.

---
+++ webpack-dev-server/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'js', 'index.js'),
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack Dev Server',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/index.css',
+    }),
+  ],
+};
